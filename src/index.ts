a8d0d60450f602f922ac4a7295851dea16e104c0
Peter Szombati <szombatipeti7@gmail.com>
2019-06-15 19:38:04 +0200

    add Logger4Interface to exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import {Logger4} from "./core/Logger4";
+import {Logger4, Logger4Interface} from "./core/Logger4";
 export default Logger4;
 
 import {EmptyLogger} from "./core/EmptyLogger";
-export {EmptyLogger};
+export {EmptyLogger, Logger4Interface};
