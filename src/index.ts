ec2a45194636f199e5c3674075c3c47666e42769
Peter Szombati <szombatipeti7@gmail.com>
2021-09-20 23:27:47 +0200

    index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@ import {Logger4, Logger4Interface} from './core/v1/Logger4'
 import {EmptyLogger} from './core/v1/EmptyLogger'
 import {parseError} from './utils/parseError'
 import {isNodeJS} from './utils/isNodeJS'
+import {Logger4V2, Logger4V2Constructor} from './core/v2/Logger4'
+import {EmptyLoggerV2} from './core/v2/EmptyLogger'
 
 export default Logger4
 
-export {EmptyLogger, Logger4Interface, parseError, isNodeJS}
+export {EmptyLogger, Logger4Interface, parseError, isNodeJS, Logger4V2, EmptyLoggerV2, Logger4V2Constructor}
