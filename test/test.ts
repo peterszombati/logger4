b8ff4f4e725a9a30a36c2acfdbc908e4ebabea4c
Peter Szombati <szombatipeti7@gmail.com>
2021-09-20 23:33:53 +0200

    test.ts

--- test/test.ts
+++ test/test.ts
@@ -1,16 +1,28 @@
 import * as path from 'path'
 import Logger4 from '../build'
+import {Logger4V2} from "../src";
+import {Writable} from "stream";
 
 describe('sandbox', () => {
-    it('sandbox', async () => {
-        let Logger = new Logger4({
-            printEnabled: true,
-            path: path.join(process.cwd(), 'log'),
-            directorySizeLimitMB: 3000,
-            savingEnabled: true
-        })
-
-        Logger.info('Hello World!')
-        Logger.info('{[0]} {[1]}', 'p1', 'p2')
+  it('#0: V1 test', async () => {
+    let Logger = new Logger4({
+      printEnabled: true,
+      path: path.join(process.cwd(), 'log'),
+      directorySizeLimitMB: 3000,
+      savingEnabled: true
     })
+
+    Logger.info('Hello World!')
+    Logger.info('{[0]} {[1]}', 'p1', 'p2')
+  })
+
+  it('#1: V2 test', async () => {
+    const logger = new Logger4V2()
+    const info = new Writable()
+    info._write = ((chunk, encoding, callback) => {
+      console.log(chunk.toString())
+    })
+    logger.on('info', info)
+    logger.info('asd')
+  })
 })
