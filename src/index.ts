301cab7b604c6382995e77e2dacf47406909a0b4
Peter Szombati <szombatipeti7@gmail.com>
2021-09-22 21:22:57 +0200

    update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,19 @@ import {parseError} from './utils/parseError'
 import {isNodeJS} from './utils/isNodeJS'
 import {Logger4V2, Logger4V2Constructor} from './core/v2/Logger4'
 import {EmptyLoggerV2} from './core/v2/EmptyLogger'
+import {JsonError} from './utils/JsonError'
+import {isObject} from './utils/isObject'
 
 export default Logger4
 
-export {EmptyLogger, Logger4Interface, parseError, isNodeJS, Logger4V2, EmptyLoggerV2, Logger4V2Constructor}
+export {
+  EmptyLogger,
+  Logger4Interface,
+  parseError,
+  isNodeJS,
+  Logger4V2,
+  EmptyLoggerV2,
+  Logger4V2Constructor,
+  isObject,
+  JsonError
+}
