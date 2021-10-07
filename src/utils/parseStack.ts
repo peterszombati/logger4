27402c537c24b80fbbc858c487b16d0f34b8764c
Peter Szombati <szombatipeti7@gmail.com>
2021-10-07 22:26:07 +0200

    hot fix

--- src/utils/parseStack.ts
+++ src/utils/parseStack.ts
@@ -12,9 +12,15 @@ const cwd = isNodeJS() ? process.cwd() : ''
 const pattern1 = new RegExp(escapeRegExp(cwd), 'g')
 const pattern2 = new RegExp('(%{cwd})(.+?)(?=:[0-9]+:[0-9]+)(:[0-9]+)(:[0-9]+)', 'g')
 
-export function parseStack(stack: string): string[] {
+function parseStackNodeJS(stack: string): string[] {
   return stack.replace(pattern1, '%{cwd}').split(/\r?\n/)
     .map(i => i.match(pattern2))
     .filter(notNull)
     .flat()
 }
+
+function parseStackJavaScript(stack: string): string[] {
+  return stack.split(/\r?\n/).slice(1).map(l => l.trim())
+}
+
+export const parseStack = isNodeJS() ? parseStackNodeJS : parseStackJavaScript
